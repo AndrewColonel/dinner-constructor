f5de6c096f24bd57284bef802331e2bbd7c4e04d
AndrewColonel <andrewcolonel@yandex.ru>
2024-11-10 22:28:30 +0300

    Реализован метод добавления блюда

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,34 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DinnerConstructor {
+    HashMap<String, ArrayList<String>> dishTypeToMenu;
+
+    public DinnerConstructor() {
+        dishTypeToMenu = new HashMap<>();
+    }
+
+   /* public void addDinnerMenu(String dishType, String dishName) {
+        if (!dishTypeToMenu.containsKey(dishType)) {
+            dishTypeToMenu.put(dishType, new ArrayList<>());
+        }
+        ArrayList<String> dishNames = new ArrayList<>();
+        dishNames.add(dishName);
+        dishTypeToMenu.put(dishType, dishNames);
+    }*/
+
+    public void addDinnerMenu(String dishType, String dishName) {
+        if (dishTypeToMenu.containsKey(dishType)) {
+            ArrayList<String> dishNames = dishTypeToMenu.get(dishType);
+            dishNames.add(dishName);
+        } else {
+            ArrayList<String> dishNames = new ArrayList<>();
+            dishNames.add(dishName);
+            dishTypeToMenu.put(dishType, dishNames);
+        }
+    }
+
 
 }
